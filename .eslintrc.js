45ae793b6745a5855f1eb4078c7ccc1687f68c0b
Mardoniosc <Mardonio@live.com>
2019-12-03 20:35:45 -0300

    configurado a estrutura do redux

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     'prettier'
   ],
   rules: {
+    'react/state-in-constructor': 'off',
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
     'import/prefer-default-export': 'off',
