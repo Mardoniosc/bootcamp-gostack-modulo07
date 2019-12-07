5f8db02c75116cb0f0e89310e42eb2fe9ee6b911
Mardoniosc <Mardonio@live.com>
2019-12-07 15:04:02 -0300

    Feito configurações de action com redux e saga

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -11,13 +11,13 @@ import * as ActionToCart from '../../store/modules/Cart/actions';
 
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart, removeFromCart, updateAmount, total }) {
+function Cart({ cart, removeFromCart, updateAmountRequest, total }) {
   function increment(product) {
-    updateAmount(product.id, product.amount + 1);
+    updateAmountRequest(product.id, product.amount + 1);
   }
 
   function decrement(product) {
-    updateAmount(product.id, product.amount - 1);
+    updateAmountRequest(product.id, product.amount - 1);
   }
 
   return (
